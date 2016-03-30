050a2df4077dd19ee3bca6a5e2585b9decf7a7e1
ChristophSchauer <schauer.christoph@gmx.at>
2016-03-30 09:56:00 +0200

    update main_RPG.py;

--- main_RPG.py
+++ main_RPG.py
@@ -6,19 +6,25 @@ Version: 0.1
 
 History:
 [2016.03.23, CS]:   initial setup; copy the main code from: 
-                    http://usingpython.com/python-rpg-game/; insert a second and 
-                    a third floor above the hall; insert items in the rooms;
-                    insert enemy; insert princess;
+                    http://usingpython.com/python-rpg-game/; insert a second 
+                    and a third floor above the hall; insert items in the 
+                    rooms; insert enemy; insert princess;
 [2016.03.24, CS]:   insert game logic (need a key for the towerroom door) and 
                     the use of the key; insert the exit function; put every 
                     command in an own function; save playerstatus; load rooms 
-                    from an external file or use the dummy file;                    
+                    from an external file or use the dummy file; 
+[2016.03.29, CS]:   ERROR5: using a command like 'fight' or 'go' without a 
+                    specific target -> crash;
+                    ERROR5: solved: the commands with two parameters are
+                    checked for length (if len(move) == 2);
+                    ERROR6: using a command like 'fight' with a false target 
+                    -> crash;
+                    ERROR6: solved: look, if the person the player wants to
+                    fight is in the room;
 """
 # import the functions
 import functions_RPG
 import parameter_enemies_RPG
-
-path_at_work = r'C:\Users\Schauer\Documents\Privat\RPG-Ratten.git\trunk'
             
 def fct_main(currentRoom, inventory , turn, rooms):
     
@@ -37,42 +43,58 @@ def fct_main(currentRoom, inventory , turn, rooms):
         # ['go','east']
         move = input(">").lower().split()
         
-        # if they type 'go' first
-        if move[0] == "go":
-            if parameter_enemies_RPG.directions.count(move[1]) == 1:
-                currentRoom = functions_RPG.fct_move(move[1], currentRoom, rooms, inventory)
-                turn += 1
-            else:
-                print("burn the heretic, this is no legal direction!")
-            
-        # if they type 'get' first
-        elif move[0] == "get":
-            inventory = functions_RPG.fct_get(move[1], currentRoom, rooms, inventory)
+        # check if there is any input
+        if len(move) != 0:
+            # if they type 'go' first
+            if move[0] == "go":
+                if len(move) == 2:
+                    if parameter_enemies_RPG.directions.count(move[1]) == 1:
+                        currentRoom = functions_RPG.fct_move(move[1], currentRoom, rooms, inventory)
+                        turn += 1
+                    else:
+                        print("burn the heretic, this is no legal direction!")
+                else: 
+                    print("falsche Eingabe")
                 
-        # if they type 'fight' first
-        elif move[0] == "fight":
-            #functions_RPG.fct_fight(move[1], currentRoom, rooms, inventory, turn)
-            functions_RPG.fct_fight_rat(playerstatus, parameter_enemies_RPG.enemystatus, move[1], currentRoom, rooms)
+            # if they type 'get' first
+            elif move[0] == "get":
+                if len(move) == 2:
+                    inventory = functions_RPG.fct_get(move[1], currentRoom, rooms, inventory)
+                else: 
+                    print("falsche Eingabe")
                     
-        # if the player wants to drop something
-        elif move[0] == "drop":
-            inventory = functions_RPG.fct_drop(move[1], currentRoom, rooms, inventory)
-        
-        #if the player wants to know his status
-        elif move[0] == "status":
-            print(playerstatus)
+            # if they type 'fight' first
+            elif move[0] == "fight":
+                if len(move) == 2:               
+                    functions_RPG.fct_fight(move[1], currentRoom, rooms, inventory, turn)
+                    #functions_RPG.fct_fight_rat(playerstatus, parameter_enemies_RPG.enemystatus, move[1], currentRoom, rooms)
+                else: 
+                    print("falsche Eingabe")
+                        
+            # if the player wants to drop something
+            elif move[0] == "drop":
+                if len(move) == 2:
+                    inventory = functions_RPG.fct_drop(move[1], currentRoom, rooms, inventory)
+                else: 
+                    print("falsche Eingabe")
             
-        # if the player wants to end the game        
-        elif move[0] == "exit":
-            functions_RPG.fct_exit(turn, playerstatus)
-            
-        # if the player wants to end the game        
-        elif move[0] == "mission":
-            print(rooms[00]["mission"])   
-            
-        # if there is a false input from the player
-        else: 
-            print("falsche Eingabe")
+            #if the player wants to know his status
+            elif move[0] == "status":
+                print(playerstatus)
+                
+            # if the player wants to end the game        
+            elif move[0] == "exit":
+                functions_RPG.fct_exit(turn, playerstatus)
+                
+            # if the player wants to end the game        
+            elif move[0] == "mission":
+                print(rooms[00]["mission"])   
+                
+            # if there is a false input from the player
+            else: 
+                print("falsche Eingabe")
+        else:
+            print("should I try something? no, this is your adventure")
            
 # main function
 if __name__=='__main__':
@@ -83,6 +105,6 @@ if __name__=='__main__':
     # initialize the turns
     turn = 1
     # generate rooms
-    rooms = functions_RPG.fct_rooms(path_at_work)
+    rooms = functions_RPG.fct_rooms()
     # start the game
     fct_main(currentRoom, inventory, turn, rooms)
