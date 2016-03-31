0eff54be0ed02583885827d0474a106481ff0e9b
ChristophSchauer <schauer.christoph@gmx.at>
2016-03-31 21:24:47 +0200

    work in GUI: scrollbar of history;

--- GUI.py
+++ GUI.py
@@ -9,6 +9,13 @@ History:
 [2016.03.31, CS]:   start with the basic layout of the GUI; insert the status, 
                     insert the commands, insert a input field, insert a button,
                     insert a label field;
+                    ERROR#8: make a history of commands given to the program 
+                    by the player;
+                    
+Sources:
+    http://sebsauvage.net/python/gui/#add_text_entry
+    http://effbot.org/tkinterbook/scrollbar.htm
+    http://effbot.org/tkinterbook/listbox.htm
 """
 
 #!/usr/bin/python
@@ -16,7 +23,7 @@ History:
 
 import tkinter as Tkinter
 
-class simpleapp_tk(Tkinter.Tk):
+class simpleapp_tk(Tkinter.Tk):        
     def __init__(self,parent):
         Tkinter.Tk.__init__(self,parent)
         self.parent = parent
@@ -28,14 +35,15 @@ class simpleapp_tk(Tkinter.Tk):
         self.geometry("600x400")
         # set the window icon, must be in the same folder as the script
         self.wm_iconbitmap('logo_black.ico')
+
         
         # status of the player
-        self.label = Tkinter.Label(self, text="here would be the player status")
-        self.label.grid(column=0,row=0)
+        label = Tkinter.Label(self, text="here would be the player status")
+        label.grid(column=0,row=0)
         
         # available commands
-        self.label = Tkinter.Label(self, text="commands:\n'exit'\n'status'\n'mission'\n'go [north, east, south, west, up, down]'\n'get [item]'\n'fight [person]'\n'drop [item]'",justify='left')
-        self.label.grid(column=1,row=0)
+        label = Tkinter.Label(self, text="commands:\n'exit'\n'status'\n'mission'\n'go [north, east, south, west, up, down]'\n'get [item]'\n'fight [person]'\n'drop [item]'",justify='left')
+        label.grid(column=1,row=0)
 
         self.entryVariable = Tkinter.StringVar()
         self.entry = Tkinter.Entry(self,textvariable=self.entryVariable)
@@ -46,13 +54,24 @@ class simpleapp_tk(Tkinter.Tk):
         button = Tkinter.Button(self,text=u"Click me !",
                                 command=self.OnButtonClick)
         button.grid(column=1,row=1,sticky='EW')
-
+        
         self.labelVariable = Tkinter.StringVar()
         label = Tkinter.Label(self,textvariable=self.labelVariable,
-                              anchor="w",fg="white",bg="blue")
+                              anchor="nw",fg="white",bg="blue",height=50)
         label.grid(column=0,row=2,columnspan=2,sticky='EW')
         self.labelVariable.set(u"Hello !")
-
+        
+        # Versuch der Scrollbar Anfang
+        scrollbar = Tkinter.Scrollbar(label)
+        scrollbar.pack(side=Tkinter.RIGHT, fill=Tkinter.Y)
+        history = Tkinter.Listbox(label, yscrollcommand=scrollbar.set)
+        #for i in range(100):
+        #    listbox.insert(Tkinter.END, str(i))
+        history.insert(Tkinter.END, self.entryVariable.get())
+        history.pack(side=Tkinter.RIGHT, fill=Tkinter.BOTH)
+        scrollbar.config(command=history.yview)
+        # Versuch der Scrollbar Ende
+        
         self.grid_columnconfigure(0,weight=1)
         # first is x-axis, second y-axis
         self.resizable(False,False)
@@ -64,12 +83,12 @@ class simpleapp_tk(Tkinter.Tk):
     def OnButtonClick(self):
         self.OnPressEnter(self)
 
-    def OnPressEnter(self,event):
-        self.labelVariable.set( self.entryVariable.get()+" (You pressed ENTER)" )
+    def OnPressEnter(self, event):
+        self.labelVariable.set(self.entryVariable.get()+" (You pressed ENTER)" )
         self.entry.focus_set()
         self.entry.selection_range(0, Tkinter.END)
 
-if __name__ == "__main__":
+if __name__ == "__main__":       
     app = simpleapp_tk(None)
     app.title('GUI: Ratten!')
     app.mainloop()
