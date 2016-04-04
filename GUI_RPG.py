a8d7c5fbf7b27dfc8be29c979e2945f36780dc8d
ChristophSchauer <schauer.christoph@gmx.at>
2016-04-04 15:55:21 +0200

    GUI programming;

--- GUI_RPG.py
+++ GUI_RPG.py
@@ -12,13 +12,19 @@ History:
                     ERROR#8: make a history of commands given to the program 
                     by the player;
                     scrollbar postponed to 'GUI - version 2';
-[2016.04.01, CS]:   
+[2016.04.04, CS]:   add the second window with the loplevel;
+                    update der name variable funzt nicht;
                     
 Sources:
     http://sebsauvage.net/python/gui/#add_text_entry
     http://effbot.org/tkinterbook/scrollbar.htm
     http://effbot.org/tkinterbook/listbox.htm
+    
+    http://www.tutorialspoint.com/python/python_gui_programming.htm
+    http://infohost.nmt.edu/tcc/help/pubs/tkinter/web/index.html
 """
+
+
 
 import tkinter
 
@@ -31,20 +37,7 @@ def game_init():
     
     root.grid()    
     
-    tkinter.Label(window, text='Name your Hero please:').grid(sticky='EW')
-    playerstatus_dummy = {
-        "name"      : [],
-        "clever"    : [],
-        "social"    : [],
-        "strong"    : [],
-        "fast"      : [],
-        "life"      : [],
-        "tricks"    : [],
-        "talents"   : [],
-        "pack"      : [],
-        "pros"      : [],
-        "cons"      : []
-                         }    
+    tkinter.Label(window, text='Name your Hero please:').grid(sticky='EW')   
     
     print("Name your Hero please:")
     playerstatus_dummy["name"] = input(">")
@@ -92,16 +85,93 @@ def simpleapp_tk(playerstatus):
         tkinter.Label(root, text=playerstatus[text[index]],anchor='w', width=60).grid(column=1,row=index,sticky='w') 
         
     root.mainloop()
+ 
+'''
+new start
+'''
+   
+global playerstatus 
+playerstatus_global = {
+        "name"      : 'global',
+        "clever"    : [],
+        "social"    : [],
+        "strong"    : [],
+        "fast"      : [],
+        "life"      : [],
+        "tricks"    : [],
+        "talents"   : [],
+        "pack"      : [],
+        "pros"      : [],
+        "cons"      : []
+                         }  
+    
+class main_window:
+    def __init__(self, master):
+        self.master = master
+        # The Frame widget is used as a container widget to organize other widgets.
+        self.frame = tkinter.Frame(self.master)
+        var = tkinter.StringVar()
+        var.set(playerstatus_global['name'])
+        self.name_lbl = tkinter.Label(self.frame, textvariable = var)
+        self.button1 = tkinter.Button(self.frame, text='click me' , width = 25, command = self.new_window)
+        # pack: This geometry manager organizes widgets in blocks before placing
+        self.name_lbl.pack()
+        self.button1.pack()
+        self.frame.pack()
+        #self.name_lbl.config(textvariable = var)
+                         
+    def new_window(self):
+        self.newWindow = tkinter.Toplevel(self.master)
+        self.app = char_gen(self.newWindow)
 
-if __name__ == "__main__":   
+class char_gen:
+
+    def __init__(self, master):
+        self.master = master
+        
+        # The Frame widget is used as a container widget to organize other widgets.
+        self.frame = tkinter.Frame(self.master)
+        #The Label widget is used to provide a single-line caption for other widgets. It can also contain images.
+        # text: To display one or more lines of text in a label widget, Internal newlines ("\n") will force a line break.
+        # bd: The size of the border around the indicator. Default is 2 pixels.
+        # wraplength: limit the number of characters in each line by setting this option to the desired number
+        var = tkinter.StringVar()
+        var.set("used for the char gen!\nname hero:")
+        self.lbl = tkinter.Label(self.frame, textvariable=var, bd=5, wraplength=200)
+        # Entry: widget is used to display a single-line text field for accepting values from a user.
+        self.name_entry = tkinter.Entry(self.frame)
+        self.quitButton = tkinter.Button(self.frame, text = 'set name', width = 25, command = self.close_windows)
+        # pack: This geometry manager organizes widgets in blocks before placing        
+        self.lbl.pack()
+        self.name_entry.pack(side=tkinter.LEFT)
+        self.quitButton.pack()
+        self.frame.pack()
+        self.lbl.update_idletasks()
+        
+    def close_windows(self):
+        playerstatus_global['name'] = self.name_entry.get()
+        self.master.destroy()
+    
+def main():
     # start the player in room 1
     currentRoom = 11
     # an inventory, which is initially empty
     inventory = []
     # initialize the turns
     turn = 1
+    root = tkinter.Tk()
+    app = main_window(root)
+    # grid: This geometry manager organizes widgets in a table-like structure 
+    # in the parent widget. 
+    # them in the parent widget
+    #root.grid()
     
-    playerstatus = game_init()
+
+    root.mainloop()
+
+if __name__ == "__main__":   
+    #playerstatus = game_init()
     
-    simpleapp_tk(playerstatus)
+    main()
+    #simpleapp_tk(playerstatus)
     
